e160567bdbf7a2505eb2111530e9fd2ab53df64c
Leninmld <150274041+Leninmld@users.noreply.github.com>
2023-11-29 19:06:07 -0500

    Add files via upload

---
+++ Lemi.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.lemi;
+
+/**
+ *
+ * @author pc
+ */
+public class Lemi {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Persona per1 = new Persona("Lenin", 18);
+        per1.saltar();
+        per1.comer();
+        per1.correr();
+    }
+}
